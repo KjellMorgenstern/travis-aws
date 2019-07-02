1197c24e9b5a44650f57a0f11132684c34f6ffed
Kjell Morgenstern <kjell@gmx.de>
2019-07-02 11:50:00 +0200

    guess 5

--- release_fritzing.bat
+++ release_fritzing.bat
@@ -1,7 +1,10 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
+
 set DESTDIR=..\release123
 rem get the absolute path of DESTDIR
 pushd %DESTDIR%
 set DESTDIR=%CD%
 popd
 
+md "%DESTDIR%"
 echo "HALLO" > "%DESTDIR%\fritzing.pc.zip"
