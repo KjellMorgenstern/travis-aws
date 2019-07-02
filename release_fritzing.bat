30a23707e29eb94b0069d3cf82fff755afa25d89
Kjell Morgenstern <kjell@gmx.de>
2019-07-02 12:21:58 +0200

    guess 6

--- release_fritzing.bat
+++ release_fritzing.bat
@@ -1,5 +1,6 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
 
+
+cd /d %~dp0
 set DESTDIR=..\release123
 rem get the absolute path of DESTDIR
 pushd %DESTDIR%
