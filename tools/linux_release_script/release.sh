e4567cb6bc7704f2e276ac9f0925604a5f6b6971
Kjell Morgenstern <kjell@gmx.de>
2019-08-06 12:39:34 +0200

    Testing fedora user config

---
+++ tools/linux_release_script/release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euE -o functrace
+failure() {
+  local err=$?
+  local lineno=$1
+  local msg=$2
+  echo "Failed at $lineno: $msg"
+  exit $err
+}
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+id
+echo $UID
+
+touch test.txt
+echo "TEST" >> test.txt
+
+ls .
+
+ls ..
+
+lrelease -version
+
+
+lrelease phoenix.pro
+
+
